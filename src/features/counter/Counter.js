972aecf773363092d1b42507c7f5e9913d49c45c
StuartCowley <voidstalker@live.co.uk>
2023-04-25 14:19:15 +0100

    Added incrementByValue and Reset counter functions

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./CounterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./CounterSlice";
 
 const Counter = () => {
+  const [value, setValue] = useState(0);
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const handleReset = () => {
+    setValue(0);
+    dispatch(reset());
+  };
 
   return (
     <div>
       <p>{count}</p>
       <div>
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={() => dispatch(incrementByAmount(Number(value)))}>
+          Update by value
+        </button>
       </div>
     </div>
   );
