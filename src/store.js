3233911b7edc5f583531d919ae93e543de04f1a5
StuartCowley <voidstalker@live.co.uk>
2023-04-25 14:32:07 +0100

    Added the API service to the store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import counterReducer from "./features/counter/CounterSlice";
+import { brewdogApi } from "./services/brewdog";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [brewdogApi.reducerPath]: brewdogApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(brewdogApi.middleware),
 });
+
+setupListeners(store.dispatch);
