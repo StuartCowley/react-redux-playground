4f55491d4416930b54c668aa463eae947a369875
StuartCowley <voidstalker@live.co.uk>
2023-04-25 14:09:35 +0100

    Created Counter component, imported reducer into store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="app">
       <h1>React redux playground</h1>
+      <Counter />
     </div>
   );
 }
