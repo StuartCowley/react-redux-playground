3d84a6f370519f769527bf202cf4bb5fcd88474b
StuartCowley <voidstalker@live.co.uk>
2023-05-15 11:27:52 +0100

    Added TodoList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./features/counter/Counter";
 import RandomBeers from "./features/randomBeer/RandomBeers";
+import TodoList from "./features/todos/TodoList";
 
 function App() {
   return (
     <div className="app">
       <h1>React redux playground</h1>
+      <TodoList />
       <Counter />
       <RandomBeers />
     </div>
