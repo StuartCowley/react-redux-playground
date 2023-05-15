1b9bd3df8267ef8806ed36ffbd24f96d8915e527
StuartCowley <voidstalker@live.co.uk>
2023-05-15 17:10:23 +0100

    Added cache invalidation tag for todos

--- src/services/todos.js
+++ src/services/todos.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const todosApi = createApi({
   reducerPath: "todosApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500/" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "todos",
+      providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
       query: (todo) => ({
@@ -13,6 +15,7 @@ export const todosApi = createApi({
         method: "POST",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
